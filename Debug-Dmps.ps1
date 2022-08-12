ea9fee760a614286ab1ce5e667e982281fb70706
PipeItToDevNull <25917750+PipeItToDevNull@users.noreply.github.com>
2022-08-12 13:56:16 -0400

    automated log creation

---
+++ Debug-Dmps.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+  Use WinDbgX to analyze .dmp files in bulk.
+.DESCRIPTION
+  using WinDbgX parse .dmp files in a specified directory and put their output into JSON arrays that can be used in other processes.
+.PARAMETER <Parameter_Name>
+  <Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+  .dmp files
+.OUTPUTS
+  ./dmps.json
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#----------[Initialisations]----------#
+
+param (
+    [Parameter(Mandatory=$True)]
+    [Object]$Directory
+)
+
+#----------[Declarations]----------#
+
+$jsonFile = './dmps.json'
+
+#----------[Functions]----------#
+
+Function Run {
+    $dmps = Get-ChildItem $Directory
+    ForEach ($dmp in $dmps) {
+        $logFile = $dmp.Name + ".log"
+        WinDbgX.exe -z "$Directory\$dmp" -c "!analyze -v ; .detach" -loga "$Directory\$logFile"
+    }
+}
+
+#----------[Execution]----------#
+Run
